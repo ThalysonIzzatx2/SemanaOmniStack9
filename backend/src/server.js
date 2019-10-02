fb298bdc5cf02ae521a1487b7c37f4d3074433ed
williamjesus <williamjesusti@gmail.com>
2019-10-02 06:50:32 -0300

    backend da aplicação

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+//Principais métodos HTTP
+//GET, POST, PUT, DELETE
+
+//Uso de Parametros via req
+//req.query => acessar query params (para filtros)
+//req.params => acessar route params (para edição e delete)
+//req.body => acessar o corpo da requisição (para criação e edição)
+
+mongoose.connect('mongodb+srv://william:semana@projects-p4icg.mongodb.net/aircnc?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
